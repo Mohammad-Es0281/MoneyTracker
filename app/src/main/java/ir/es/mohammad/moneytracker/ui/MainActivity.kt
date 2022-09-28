0c020901a825d6f847a938e19684276fcc64668f
Mohammad_Es0281 <mohammad.es0281@gmail.com>
2022-09-28 13:45:34 +0330

    Add splash screen and change icon

--- app/src/main/java/ir/es/mohammad/moneytracker/ui/MainActivity.kt
+++ app/src/main/java/ir/es/mohammad/moneytracker/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package ir.es.mohammad.moneytracker.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
@@ -12,6 +13,7 @@ import ir.es.mohammad.moneytracker.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         AppCenter.start(application, "cdc9cbbf-9a1f-464a-9de9-3ebcee8b70fe",
