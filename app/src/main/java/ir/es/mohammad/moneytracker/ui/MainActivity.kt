3f436bf20ed880cf0e8e657cdc419d379a776622
Mohammad_Es0281 <mohammad.es0281@gmail.com>
2022-09-28 11:38:04 +0330

    Add app center

--- app/src/main/java/ir/es/mohammad/moneytracker/ui/MainActivity.kt
+++ app/src/main/java/ir/es/mohammad/moneytracker/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package ir.es.mohammad.moneytracker.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.AndroidEntryPoint
 import ir.es.mohammad.moneytracker.R
 
@@ -10,5 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "cdc9cbbf-9a1f-464a-9de9-3ebcee8b70fe",
+            Analytics::class.java, Crashes::class.java)
     }
 }
